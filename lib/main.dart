d996d59a80ab65b441f1ddc715d3131c085aff7b
Sunilnani <42605686+Sunilnani@users.noreply.github.com>
2020-12-17 08:21:13 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,306 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app100/profile.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> image=["img/heart.png","img/digestive.png","img/brain.png",];
+  List<String> title=["Cardiology","Gastrology","Neurology"];
+  List<Color> colors=[Color(0xFFa7c4e1),Color(0xFF91d4da),Color(0xFFf2c7a7)];
+  List<String> doctorimage=["img/mendoctor.png","img/tomheart.png","img/womendoctor.png"];
+  List<String> name=["Prof.Dr.Kevin Williams","Dr.Jane Foster","Tommy Smith"];
+  List<String> profession=["Heart Surgeon-United hospital","digestive Surgeon-United hospital","brain Surgeon-United hospital"];
+  List<String> appointmentcost=["10.50","10.40","12.00"];
+  String istapped="";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height:10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          child:Icon(Icons.menu_outlined,color: Color(0xFF868fa2),)
+                      ),
+                      Container(
+                        child: CircleAvatar(
+                          backgroundImage:AssetImage("img/women1.png",),
+                          backgroundColor: Color(0xFF9dd6dd),
+                          radius: 20,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 15,),
+                  Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text("Hello Jane",style: TextStyle(color: Color(0xFF334767),fontWeight: FontWeight.w700,fontSize: 17),)
+                  ),
+                  SizedBox(height: 12,),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Find Your Specialist",style: TextStyle(color: Color(0xFF35496a),fontSize: 28,fontWeight: FontWeight.w700),),
+                  ),
+                  SizedBox(height: 25,),
+                  Stack(
+
+                    children: <Widget>[
+                      Container(
+                        height:50,
+                        child:TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: Icon(Icons.search,size: 25,color: Colors.black12,),
+                            hintText: "Search doctor",
+                            hintStyle: TextStyle(color: Color(0xFFe6e7ea),
+                                fontSize: 15),
+
+                            enabledBorder: OutlineInputBorder(
+
+                                borderRadius: BorderRadius.circular(25),
+                                borderSide: BorderSide(
+
+                                    color: Colors.white
+                                )
+                            ),
+                          ),
+                        ),
+
+                      ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: CircleAvatar(
+                          backgroundColor: Color(0xFFa3d7dc),
+                          backgroundImage: AssetImage("img/loading.png"),
+                          radius: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Container(
+                    height: 120,
+
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: title.length,
+                      itemBuilder: (context,index){
+                        return Humanparts(
+                          image: image[index],
+                          title: title[index],
+                          colors: colors[index],
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Text("Top Doctor",style: TextStyle(color: Color(0xFF6c7892),fontWeight: FontWeight.w700,fontSize: 22),),
+                      ),
+                      Container(
+                        child: Text("See all",style: TextStyle(color: Color(0xFFaadbe2),fontWeight: FontWeight.w500),),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Container(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      itemCount: name.length,
+                      itemBuilder: (context, index){
+                        return InkWell(
+                          onTap: (){
+                            Navigator.push(context,
+                            MaterialPageRoute(builder: (context)=>Profile(
+                              name[index]
+                            )));
+                          },
+                          child: DoctorsAppointment(
+                            image: doctorimage[index],
+                            title: name[index],
+                            subtitle: profession[index],
+                            content: appointmentcost[index],
+                            colors: colors[index],
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DoctorsAppointment extends StatelessWidget {
+  const DoctorsAppointment({
+    this.image,
+    this.name,
+    this.title,
+    this.subtitle,
+    this.content,
+    this.colors,
+    Key key,
+  }) : super(key: key);
+  final String image;
+  final String name;
+  final String title;
+  final String subtitle;
+  final String content;
+  final Color colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+          alignment: Alignment.topLeft,
+          height: 100,
+          width: 300,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white
+          ),
+          child:Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left:8.0,bottom: 12),
+                child: Container(
+                  child: CircleAvatar(
+                    backgroundColor:this.colors,
+                    backgroundImage: AssetImage(image),
+                    radius: 25,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left:18.0),
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(title,style: TextStyle(color: Color(0xFF707a8d),fontWeight: FontWeight.w500,fontSize: 15),
+                      ),
+                      SizedBox(height: 5,),
+                      Text(subtitle,style: TextStyle(color: Color(0xFFd0d4d9),fontSize: 12,fontWeight: FontWeight.w500),),
+                      SizedBox(height: 5,),
+                      Row(
+                        children: [
+                          Icon(Icons.access_time_outlined,color: Color(0xFFdef2f3),size: 10,),
+                          Text("10:40 AM - 2:40 PM",style: TextStyle(color: Color(0xFFa7adba),fontSize: 14),),
+                        ],
+                      ),
+                      SizedBox(height: 5,),
+                      Container(
+                        height: 30,
+                        width: 200,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(content,style: TextStyle(color: Color(0xFFe1e3e7)),),
+                            RaisedButton(
+                              padding: const EdgeInsets.only(left:10,right: 10),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: new BorderRadius.circular(18.0)),
+                              textColor: Colors.white,
+                              color: Color(0xFFaadbe2),
+                              onPressed: (){
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => Profile(title)
+                                  )
+                                );
+                              },
+                              child: Text('Appointment'),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+      ),
+    );
+  }
+}
+
+class Humanparts extends StatelessWidget {
+  const Humanparts({
+    this.image,
+    this.title,
+    this.colors,
+    Key key,
+  }) : super(key: key);
+  final String image;
+  final String title;
+  final Color colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Container(
+              height: 100,
+              width: 100,
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              decoration: BoxDecoration(
+                color:this.colors,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: this.colors,
+                    offset: const Offset(
+                      1.0,
+                      1.0,
+                    ),
+                    blurRadius: 3.0,
+                    spreadRadius: 2.0,
+                  ),
+                ],
+              ),
+              child:Column(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(top:8.0),
+                      child: Image.asset(image,fit: BoxFit.cover,height:60,)
+                  ),
+                  SizedBox(height: 4,),
+                  Text(title,style: TextStyle(color: Colors.white),)
+                ],
+              )
+
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/profile.dart
@@ -0,0 +1,290 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  final String name;
+  Profile(this.name);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<Profile> {
+  List selectedIndex =[];
+  List<String> day=["Sun","Mon","Tue","Wed","Thu"];
+  List<String> date=["2","3","4","5","6"];
+  List<Color> colors=[Colors.white,Colors.white,Color(0xFF94d3da),Colors.white,Colors.white];
+  List <String> count=["648","6","1084","5"];
+  List<String> operations=["Patients","Years Exp","Surgery","Rating"];
+  List<Color> color=[Color(0xFF8db4db),Color(0xFF94d3da),Color(0xFFf7d2b8),Color(0xFFf5bc77)];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 1),
+            child: Container(
+              child: SingleChildScrollView(
+                child: Stack(
+                  children: [
+                    Container(
+                        height: 400,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Color(0xFFf2c6a9),
+                        ),
+                        child:Column(
+                          children: [
+                            SizedBox(height: 25,),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(icon: Icon(Icons.arrow_back_ios_outlined,color: Color(0xFFfdf3f0),),
+                                    onPressed: (){
+                                  Navigator.pop(context);
+                                    }),
+                                Padding(
+                                  padding: const EdgeInsets.only(right:8.0),
+                                  child: Icon(Icons.menu,color: Color(0xFFfdf3f0),),
+                                )
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 50),
+                              child: Center(child: Image.asset("img/doctors.png",height: 300,)),
+                            )
+                          ],
+                        )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top:350.0),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Color(0xFFffffff),
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(height: 70,),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("Appointment",style: TextStyle(color: Color(0xFF4c5d78),fontWeight: FontWeight.w700,fontSize: 17),),
+                                Text("< August >",style: TextStyle(color: Color(0xFF9ea6b4),fontSize: 12,fontWeight: FontWeight.w500),)
+                              ],
+                            ),
+                            SizedBox(height: 5,),
+                            Container(
+                              height: 79,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                shrinkWrap: true,
+                                itemCount: colors.length,
+                                itemBuilder: (context,index){
+                                  final isSelected=selectedIndex.contains(index);
+                                  return GestureDetector(
+                                    onTap: (){
+                                      setState(() {
+                                        if(isSelected){
+                                          selectedIndex.remove(index);
+                                        }
+                                        else{
+                                          selectedIndex.add(index);
+                                        }
+                                      });
+                                    },
+                                    child: Weekdays(
+                                      day: day[index],
+                                      date: date[index],
+                                      color: isSelected ? Colors.red:Colors.blueGrey,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("About",style: TextStyle(color: Color(0xFF677288),fontWeight: FontWeight.w700,fontSize: 17),),
+                                Text("Call now",style: TextStyle(color: Color(0xFFbccfe6),fontSize: 15,fontWeight: FontWeight.w500),)
+                              ],
+                            ),
+                            SizedBox(height: 5,),
+                            Container(
+                              height: 50,
+                              width: 320,
+                              child: Text("Dr. Jane Faster is a long establised fact that a reader will be distracted by the readable content of a page when looking at its layout The point of using lorem.",style: TextStyle(color: Color(0xFFb4b8c2)),),
+                            ),
+                            SizedBox(height: 10,),
+                            Container(
+                              height: 79,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                shrinkWrap: true,
+                                itemCount: color.length,
+                                itemBuilder: (context,index){
+                                  return Patients(
+                                    count: count[index],
+                                    operations: operations[index],
+                                    colors: color[index],  
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top:710.0),
+                      child: Container(
+                        height: 70,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35),
+                            color: Color(0xFF94d3da)
+                        ),
+                        child:Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text("Fee: 10.50",style: TextStyle(color: Color(0xFFe2f3f7),fontWeight: FontWeight.w700,fontSize: 15),),
+                            Container(
+                              alignment: Alignment.center,
+                              height: 30,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Color(0xFFaadbe2)
+                              ),
+                              child: Text("Appointment",style: TextStyle(color: Color(0xFFe2f3f7),fontSize: 15,fontWeight: FontWeight.w600),),
+                            )
+                          ],
+                        ) ,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top:318.0),
+                      child: Center(
+                        child:Container(
+                          height: 90,
+                          width: 290,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Color(0xFF8db4db),
+                            boxShadow: [
+                              BoxShadow(
+                                color:Color(0xFFd8e4f2),
+                                offset: const Offset(
+                                  1.0,
+                                  5.0,
+                                ),
+                                blurRadius: 3.0,
+                                spreadRadius: 2.0,
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 5,),
+                              Center(
+                                child: Text(widget.name,style: TextStyle(color: Color(0xFFe9eff4),fontWeight: FontWeight.w800,fontSize: 20),),
+                              ),
+                              SizedBox(height: 3,),
+                              Text("Senior Cardiologist and Surgeon",style: TextStyle(color: Color(0xFFe9eff4),fontWeight: FontWeight.w300,fontSize: 12),),
+                              SizedBox(height: 3,),
+                              Text("United State medical college & hospital",style: TextStyle(color: Color(0xFFe9eff4),fontWeight: FontWeight.w300,fontSize: 15),),
+
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+        )
+    );
+  }
+}
+
+class Patients extends StatelessWidget {
+  const Patients({
+    this.count,
+    this.operations,
+    this.colors,
+  }) ;
+  final String count;
+  final String operations;
+  final Color colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Container(
+            height:55,
+            width: 70,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                color:this.colors
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top:10.0),
+              child: Column(
+                children: [
+                  Text(count,style: TextStyle(color: Color(0xFFdce7f3),fontWeight: FontWeight.w700),),
+                  Text(operations,style: TextStyle(color: Color(0xFFdce7f3)),)
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class Weekdays extends StatelessWidget {
+  const Weekdays({
+    this.day,
+    this.date,
+    this.color,
+    Key key,
+  }) : super(key: key);
+  final String day;
+  final String date;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            height:75,
+            width: 43,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: this.color
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top:15.0),
+              child: Column(
+                children: [
+                  Text(day,style: TextStyle(color: Color(0xFF878a98)),),
+                  Text(date,style: TextStyle(color: Color(0xFF878a98)),)
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
